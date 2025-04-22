4255e4308d779f9d2c7aaba970ab50522de3482f
Shayokh <shayokhshuvro14@gmail.com>
2025-04-22 19:35:25 +0600

    add solution for 'Rotate String 'w3resource' Periodically'

---
+++ basic/5.ts
@@ -0,0 +1,11 @@
+function rotateText() {
+    let text = "w3resource " // Extra space at the end for proper visual effect
+
+    // Rotate the text every 100ms
+    setInterval(() => {
+        text = text[text.length - 1] + text.substring(0, text.length - 1)
+        console.log(text)
+    }, 100);
+}
+
+rotateText()
